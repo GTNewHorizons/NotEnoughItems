a35aeabecd01769905c162e46b05b872b16355f7
MuXiu1997 <muxiu1997@gmail.com>
2022-05-15 14:39:10 +0800

    Use shortcut key to trigger overlay recipe

--- src/main/java/codechicken/nei/recipe/GuiCraftingRecipe.java
+++ src/main/java/codechicken/nei/recipe/GuiCraftingRecipe.java
@@ -14,59 +14,30 @@ import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.IChatComponent;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
-public class GuiCraftingRecipe extends GuiRecipe
-{
+public class GuiCraftingRecipe extends GuiRecipe {
     public static boolean openRecipeGui(String outputId, Object... results) {
         Minecraft mc = NEIClientUtils.mc();
         GuiScreen prevscreen = mc.currentScreen;// instanceof GuiContainer ? (GuiContainer) mc.currentScreen : null;
 
-        ArrayList<ICraftingHandler> handlers;
-        TaskProfiler profiler = ProfilerRecipeHandler.getProfiler();
-        profiler.start("recipe.concurrent.crafting");
-       
-        // Pre-find the fuels so we're not fighting over it
-        FuelRecipeHandler.findFuelsOnceParallel();
-
-        try {
-            handlers = serialCraftingHandlers.stream().map(h -> h.getRecipeHandler(outputId, results))
-                .filter(h -> h.numRecipes() > 0)
-                .collect(Collectors.toCollection(ArrayList::new));
-
-            handlers.addAll(ItemList.forkJoinPool.submit(() -> craftinghandlers.parallelStream()
-                .map(h -> h.getRecipeHandler(outputId, results))
-                .filter(h -> h.numRecipes() > 0)
-                .collect(Collectors.toCollection(ArrayList::new))).get());
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-            EntityPlayer player = Minecraft.getMinecraft().thePlayer;
-            if (player != null) {
-                IChatComponent chat = new ChatComponentTranslation("nei.chat.recipe.error");
-                chat.getChatStyle().setColor(EnumChatFormatting.RED);
-                player.addChatComponentMessage(chat);
-            }
-            return false;
-        } finally {
-            profiler.end();
-        }
-
-        if (handlers.isEmpty())
-            return false;
+        ArrayList<ICraftingHandler> handlers = getHandlers(outputId, null, results);
+        if (handlers == null || handlers.isEmpty()) return false;
 
         handlers.sort(NEIClientConfig.HANDLER_COMPARATOR);
 
         BookmarkRecipeId recipeId = null;
 
         if ("item".equals(outputId)) {
-            recipeId = getRecipeId(prevscreen, (ItemStack)results[0]);
+            recipeId = getRecipeId(prevscreen, (ItemStack) results[0]);
         }
 
         GuiCraftingRecipe gui = new GuiCraftingRecipe(prevscreen, handlers, recipeId);
-        
+
         mc.displayGuiScreen(gui);
 
         if (NEIClientConfig.saveCurrentRecipeInBookmarksEnabled() && !NEIClientUtils.shiftKey()) {
@@ -76,8 +47,42 @@ public class GuiCraftingRecipe extends GuiRecipe
         return true;
     }
 
-    protected static BookmarkRecipeId getRecipeId(GuiScreen gui, ItemStack stackover)
-    {
+    public static ArrayList<ICraftingHandler> getHandlers(String outputId, @Nullable String handlerId, Object... results) {
+        ArrayList<ICraftingHandler> handlers;
+        TaskProfiler profiler = ProfilerRecipeHandler.getProfiler();
+        profiler.start("recipe.concurrent.crafting");
+
+        // Pre-find the fuels so we're not fighting over it
+        FuelRecipeHandler.findFuelsOnceParallel();
+
+        try {
+            handlers = serialCraftingHandlers.stream()
+                    .filter(h -> handlerId == null || h.getHandlerId().equals(handlerId))
+                    .map(h -> h.getRecipeHandler(outputId, results))
+                    .filter(h -> h.numRecipes() > 0)
+                    .collect(Collectors.toCollection(ArrayList::new));
+
+            handlers.addAll(ItemList.forkJoinPool.submit(() -> craftinghandlers.parallelStream()
+                    .filter(h -> handlerId == null || h.getHandlerId().equals(handlerId))
+                    .map(h -> h.getRecipeHandler(outputId, results))
+                    .filter(h -> h.numRecipes() > 0)
+                    .collect(Collectors.toCollection(ArrayList::new))).get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            EntityPlayer player = Minecraft.getMinecraft().thePlayer;
+            if (player != null) {
+                IChatComponent chat = new ChatComponentTranslation("nei.chat.recipe.error");
+                chat.getChatStyle().setColor(EnumChatFormatting.RED);
+                player.addChatComponentMessage(chat);
+            }
+            return null;
+        } finally {
+            profiler.end();
+        }
+        return handlers;
+    }
+
+    protected static BookmarkRecipeId getRecipeId(GuiScreen gui, ItemStack stackover) {
 
         if (gui instanceof GuiRecipe) {
             final List<PositionedStack> ingredients = ((GuiRecipe) gui).getFocusedRecipeIngredients();
@@ -91,8 +96,7 @@ public class GuiCraftingRecipe extends GuiRecipe
         return ItemPanels.bookmarkPanel.getBookmarkRecipeId(stackover);
     }
 
-    private GuiCraftingRecipe(GuiScreen prevgui, ArrayList<ICraftingHandler> handlers, BookmarkRecipeId recipeId)
-    {
+    private GuiCraftingRecipe(GuiScreen prevgui, ArrayList<ICraftingHandler> handlers, BookmarkRecipeId recipeId) {
         this(prevgui, handlers);
         this.recipeId = recipeId;
     }
@@ -104,10 +108,10 @@ public class GuiCraftingRecipe extends GuiRecipe
 
     public static void registerRecipeHandler(ICraftingHandler handler) {
         final String handlerId = handler.getHandlerId();
-        if(craftinghandlers.stream().anyMatch(h -> h.getHandlerId().equals(handlerId)) || serialCraftingHandlers.stream().anyMatch(h -> h.getHandlerId().equals(handlerId)))
+        if (craftinghandlers.stream().anyMatch(h -> h.getHandlerId().equals(handlerId)) || serialCraftingHandlers.stream().anyMatch(h -> h.getHandlerId().equals(handlerId)))
             return;
 
-        if(NEIClientConfig.serialHandlers.contains(handlerId))
+        if (NEIClientConfig.serialHandlers.contains(handlerId))
             serialCraftingHandlers.add(handler);
         else
             craftinghandlers.add(handler);
--- src/main/java/codechicken/nei/recipe/RecipeItemInputHandler.java
+++ src/main/java/codechicken/nei/recipe/RecipeItemInputHandler.java
@@ -1,29 +1,32 @@
 package codechicken.nei.recipe;
 
+import codechicken.lib.gui.GuiDraw;
 import codechicken.nei.ItemPanels;
+import codechicken.nei.LayoutManager;
 import codechicken.nei.NEIClientConfig;
 import codechicken.nei.NEIClientUtils;
+import codechicken.nei.PositionedStack;
+import codechicken.nei.api.IOverlayHandler;
+import codechicken.nei.api.IRecipeOverlayRenderer;
 import codechicken.nei.guihook.GuiContainerManager;
 import codechicken.nei.guihook.IContainerInputHandler;
-import codechicken.nei.PositionedStack;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.item.ItemStack;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class RecipeItemInputHandler implements IContainerInputHandler
-{
+public class RecipeItemInputHandler implements IContainerInputHandler {
     @Override
-    public boolean lastKeyTyped(GuiContainer gui, char keyChar, int keyCode)
-    {
+    public boolean lastKeyTyped(GuiContainer gui, char keyChar, int keyCode) {
         ItemStack stackover = GuiContainerManager.getStackMouseOver(gui);
-        if(stackover == null)
-            return false;
+        if (stackover == null) return false;
 
-        if(keyCode == NEIClientConfig.getKeyBinding("gui.recipe"))
+        if (keyCode == NEIClientConfig.getKeyBinding("gui.recipe"))
             return GuiCraftingRecipe.openRecipeGui("item", stackover.copy());
 
-        if(keyCode == NEIClientConfig.getKeyBinding("gui.usage"))
+        if (keyCode == NEIClientConfig.getKeyBinding("gui.usage"))
             return GuiUsageRecipe.openRecipeGui("item", stackover.copy());
 
         if (keyCode == NEIClientConfig.getKeyBinding("gui.bookmark")) {
@@ -39,68 +42,84 @@ public class RecipeItemInputHandler implements IContainerInputHandler
             ItemPanels.bookmarkPanel.addOrRemoveItem(stackover.copy(), handlerName, ingredients);
         }
 
+        if (keyCode == NEIClientConfig.getKeyBinding("gui.overlay")) {
+            final Point mousePosition = GuiDraw.getMousePosition();
+            if (ItemPanels.bookmarkPanel.getStackMouseOver(mousePosition.x, mousePosition.y) == null) return false;
+            final BookmarkRecipeId recipeId = ItemPanels.bookmarkPanel.getBookmarkRecipeId(stackover);
+            if (recipeId == null || recipeId.handlerName == null || recipeId.ingredients == null || recipeId.ingredients.isEmpty())
+                return false;
+            final ArrayList<ICraftingHandler> handlers = GuiCraftingRecipe.getHandlers("item", recipeId.handlerName, stackover.copy());
+            if (handlers == null || handlers.isEmpty()) return false;
+            final ICraftingHandler handler = handlers.get(0);
+            if (recipeId.position == -1) {
+                for (int i = 0; i < handler.numRecipes(); i++) {
+                    if (recipeId.equalsIngredients(handler.getIngredientStacks(i))) {
+                        recipeId.position = i;
+                        break;
+                    }
+                }
+            }
+            if (recipeId.position == -1) return false;
+            final IRecipeOverlayRenderer renderer = handler.getOverlayRenderer(gui, recipeId.position);
+            final IOverlayHandler overlayHandler = handler.getOverlayHandler(gui, recipeId.position);
+            final boolean shift = NEIClientUtils.shiftKey();
+            if (renderer == null || shift) {
+                overlayHandler.overlayRecipe(gui, handler, recipeId.position, shift);
+            } else {
+                LayoutManager.overlayRenderer = LayoutManager.overlayRenderer == null ? renderer : null;
+            }
+        }
+
         return false;
     }
 
     @Override
-    public boolean mouseClicked(GuiContainer gui, int mousex, int mousey, int button)
-    {
+    public boolean mouseClicked(GuiContainer gui, int mousex, int mousey, int button) {
         ItemStack stackover = GuiContainerManager.getStackMouseOver(gui);
 
-        if(stackover == null)
-            return false;
+        if (stackover == null) return false;
 
-        if (!(gui instanceof GuiRecipe))
-            return false;
+        if (!(gui instanceof GuiRecipe)) return false;
 
         //disabled open recipe gui if hold shift (player have move recipe)
         if (button == 0 && ItemPanels.bookmarkPanel.getStackMouseOver(mousex, mousey) != null && NEIClientUtils.shiftKey()) {
             return false;
         }
 
-        if(button == 0)
-            return GuiCraftingRecipe.openRecipeGui("item", stackover.copy());
+        if (button == 0) return GuiCraftingRecipe.openRecipeGui("item", stackover.copy());
 
-        if(button == 1)
-            return GuiUsageRecipe.openRecipeGui("item", stackover.copy());
+        if (button == 1) return GuiUsageRecipe.openRecipeGui("item", stackover.copy());
 
         return false;
     }
 
     @Override
-    public void onKeyTyped(GuiContainer gui, char keyChar, int keyID)
-    {
+    public void onKeyTyped(GuiContainer gui, char keyChar, int keyID) {
     }
 
     @Override
-    public void onMouseClicked(GuiContainer gui, int mousex, int mousey, int button)
-    {
+    public void onMouseClicked(GuiContainer gui, int mousex, int mousey, int button) {
     }
 
     @Override
-    public void onMouseUp(GuiContainer gui, int mousex, int mousey, int button)
-    {
+    public void onMouseUp(GuiContainer gui, int mousex, int mousey, int button) {
     }
 
     @Override
-    public boolean keyTyped(GuiContainer gui, char keyChar, int keyID)
-    {
+    public boolean keyTyped(GuiContainer gui, char keyChar, int keyID) {
         return false;
     }
 
     @Override
-    public boolean mouseScrolled(GuiContainer gui, int mousex, int mousey, int scrolled)
-    {
+    public boolean mouseScrolled(GuiContainer gui, int mousex, int mousey, int scrolled) {
         return false;
     }
 
     @Override
-    public void onMouseScrolled(GuiContainer gui, int mousex, int mousey, int scrolled)
-    {
+    public void onMouseScrolled(GuiContainer gui, int mousex, int mousey, int scrolled) {
     }
 
     @Override
-    public void onMouseDragged(GuiContainer gui, int mousex, int mousey, int button, long heldTime)
-    {
+    public void onMouseDragged(GuiContainer gui, int mousex, int mousey, int button, long heldTime) {
     }
 }
