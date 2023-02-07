c233bf9ff4ac6420ec884922fd634e3866e5cf51
miozune <miozune@gmail.com>
2023-02-08 02:09:15 +0900

    better error handling

--- src/main/java/codechicken/nei/recipe/RecipeHandlerQuery.java
+++ src/main/java/codechicken/nei/recipe/RecipeHandlerQuery.java
@@ -3,7 +3,6 @@ package codechicken.nei.recipe;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -12,9 +11,6 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.IChatComponent;
-
-import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.apache.commons.lang3.tuple.Pair;
 
 import codechicken.core.TaskProfiler;
 import codechicken.nei.ItemList;
@@ -26,6 +22,7 @@ class RecipeHandlerQuery<T extends IRecipeHandler> {
     private final List<T> recipeHandlers;
     private final List<T> serialRecipeHandlers;
     private final String[] errorMessage;
+    private boolean error = false;
 
     RecipeHandlerQuery(Function<T, T> recipeHandlerFunction, List<T> recipeHandlers, List<T> serialRecipeHandlers,
             String... errorMessage) {
@@ -39,11 +36,11 @@ class RecipeHandlerQuery<T extends IRecipeHandler> {
         TaskProfiler profiler = ProfilerRecipeHandler.getProfiler();
         profiler.start(profilerSection);
         try {
-            Pair<Boolean, ArrayList<T>> handlersResult = getRecipeHandlersParallel();
-            if (handlersResult.getLeft()) {
+            ArrayList<T> handlers = getRecipeHandlersParallel();
+            if (error) {
                 displayRecipeLookupError();
             }
-            return handlersResult.getRight();
+            return handlers;
         } catch (InterruptedException | ExecutionException e) {
             printLog(e);
             displayRecipeLookupError();
@@ -53,48 +50,37 @@ class RecipeHandlerQuery<T extends IRecipeHandler> {
         }
     }
 
-    private Pair<Boolean, ArrayList<T>> getRecipeHandlersParallel() throws InterruptedException, ExecutionException {
+    private ArrayList<T> getRecipeHandlersParallel() throws InterruptedException, ExecutionException {
         // Pre-find the fuels so we're not fighting over it
         FuelRecipeHandler.findFuelsOnceParallel();
-
-        Pair<Boolean, ArrayList<T>> serialHandlersResult = getSerialHandlersWithRecipes();
-        boolean err = serialHandlersResult.getLeft();
-        ArrayList<T> ret = serialHandlersResult.getRight();
-
-        Pair<Boolean, ArrayList<T>> handlersResult = getHandlersWithRecipes();
-        err |= handlersResult.getLeft();
-        ret.addAll(handlersResult.getRight());
-
-        ret.sort(NEIClientConfig.HANDLER_COMPARATOR);
-        return new ImmutablePair<>(err, ret);
+        ArrayList<T> handlers = getSerialHandlersWithRecipes();
+        handlers.addAll(getHandlersWithRecipes());
+        handlers.sort(NEIClientConfig.HANDLER_COMPARATOR);
+        return handlers;
     }
 
-    private Pair<Boolean, ArrayList<T>> getSerialHandlersWithRecipes() {
-        AtomicBoolean err = new AtomicBoolean(false);
-        ArrayList<T> ret = serialRecipeHandlers.stream().map(handler -> {
+    private ArrayList<T> getSerialHandlersWithRecipes() {
+        return serialRecipeHandlers.stream().map(handler -> {
             try {
                 return recipeHandlerFunction.apply(handler);
             } catch (Throwable t) {
                 printLog(t);
-                err.set(true);
+                error = true;
                 return null;
             }
         }).filter(h -> h != null && h.numRecipes() > 0).collect(Collectors.toCollection(ArrayList::new));
-        return new ImmutablePair<>(err.get(), ret);
     }
 
-    private Pair<Boolean, ArrayList<T>> getHandlersWithRecipes() throws InterruptedException, ExecutionException {
-        AtomicBoolean err = new AtomicBoolean(false);
-        ArrayList<T> ret = ItemList.forkJoinPool.submit(() -> recipeHandlers.parallelStream().map(handler -> {
+    private ArrayList<T> getHandlersWithRecipes() throws InterruptedException, ExecutionException {
+        return ItemList.forkJoinPool.submit(() -> recipeHandlers.parallelStream().map(handler -> {
             try {
                 return recipeHandlerFunction.apply(handler);
             } catch (Throwable t) {
                 printLog(t);
-                err.set(true);
+                error = true;
                 return null;
             }
         }).filter(h -> h != null && h.numRecipes() > 0).collect(Collectors.toCollection(ArrayList::new))).get();
-        return new ImmutablePair<>(err.get(), ret);
     }
 
     private void printLog(Throwable t) {
