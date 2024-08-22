3e1baa4ded118dbfa0839b6247c765cfa17f4ccd
slprime <history-21@yandex.ru>
2024-08-22 22:00:11 +0300

    NEI Optimization

--- src/main/java/codechicken/nei/SearchTokenParser.java
+++ src/main/java/codechicken/nei/SearchTokenParser.java
@@ -188,7 +188,11 @@ public class SearchTokenParser {
 
             for (ISearchParserProvider _provider : getProviders()) {
                 if (_provider.getSearchMode() == SearchMode.ALWAYS) {
-                    filters.add(_provider.getFilter(token));
+                    ItemFilter filter = _provider.getFilter(token);
+
+                    if (filter != null) {
+                        filters.add(filter);
+                    }
                 }
             }
 
