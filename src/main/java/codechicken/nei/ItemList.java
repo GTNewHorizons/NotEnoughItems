37ae58f3ceb406beb3230e63a69514157e8d5f4f
lailani-f <>
2025-01-04 13:37:51 +0100

    inlined variable, reverted to single class imports

--- src/main/java/codechicken/nei/ItemList.java
+++ src/main/java/codechicken/nei/ItemList.java
@@ -1,6 +1,12 @@
 package codechicken.nei;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.ForkJoinWorkerThread;
@@ -363,8 +369,7 @@ public class ItemList {
 
             if (interrupted()) return;
 
-            HashMap<ItemStack, Integer> localOrdering = ItemList.ordering;
-            Comparator<ItemStack> comparator = Comparator.comparingInt(localOrdering::get);
+            Comparator<ItemStack> comparator = Comparator.comparingInt(ItemList.ordering::get);
             filtered.sort(comparator::compare);
 
             if (interrupted()) return;
