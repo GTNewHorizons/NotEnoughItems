85e671ca24dbbd74ac2e43c3814ed9a502b3e5f9
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-05-29 18:42:16 +0200

    fix world leak in ItemMobSpawner

--- src/main/java/codechicken/nei/SpawnerRenderer.java
+++ src/main/java/codechicken/nei/SpawnerRenderer.java
@@ -9,7 +9,6 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.boss.BossStatus;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
 import net.minecraftforge.client.IItemRenderer;
 
 import org.lwjgl.opengl.GL11;
@@ -33,31 +32,38 @@ public class SpawnerRenderer implements IItemRenderer {
         String bossName = BossStatus.bossName;
         int bossTimeout = BossStatus.statusBarTime;
         boolean bossHasColorModifier = BossStatus.hasColorModifier;
+        Entity entity = ItemMobSpawner.getEntity(meta);
         try {
-            World world = NEIClientUtils.mc().theWorld;
-            ItemMobSpawner.loadSpawners(world);
+            ItemMobSpawner.loadSpawners();
             CCRenderState.changeTexture(TextureMap.locationBlocksTexture);
             render.renderBlockAsItem(Blocks.mob_spawner, 0, 1F);
-            GL11.glPushMatrix();
 
-            Entity entity = ItemMobSpawner.getEntity(meta);
-            entity.setWorld(world);
-            float f1 = 0.4375F;
-            if (entity.getShadowSize() > 1.5) f1 = 0.1F;
-            GL11.glRotatef((float) (ClientUtils.getRenderTime() * 10), 0.0F, 1.0F, 0.0F);
-            GL11.glRotatef(-20F, 1.0F, 0.0F, 0.0F);
-            GL11.glTranslatef(0.0F, -0.4F, 0.0F);
-            GL11.glScalef(f1, f1, f1);
-            entity.setLocationAndAngles(0, 0, 0, 0.0F, 0.0F);
-            RenderManager.instance.renderEntityWithPosYaw(entity, 0.0D, 0.0D, 0.0D, 0.0F, 0);
-            GL11.glPopMatrix();
+            if (entity != null) {
+                entity.setWorld(render.minecraftRB.theWorld);
+                GL11.glPushMatrix();
+                float f1 = 0.4375F;
+                if (entity.getShadowSize() > 1.5) f1 = 0.1F;
+                GL11.glRotatef((float) (ClientUtils.getRenderTime() * 10), 0.0F, 1.0F, 0.0F);
+                GL11.glRotatef(-20F, 1.0F, 0.0F, 0.0F);
+                GL11.glTranslatef(0.0F, -0.4F, 0.0F);
+                GL11.glScalef(f1, f1, f1);
+                entity.setLocationAndAngles(0, 0, 0, 0.0F, 0.0F);
+                RenderManager.instance.renderEntityWithPosYaw(entity, 0.0D, 0.0D, 0.0D, 0.0F, 0);
+                GL11.glPopMatrix();
+                entity.setWorld(null);
+            }
 
             GL11.glEnable(GL12.GL_RESCALE_NORMAL);
             OpenGlHelper.setActiveTexture(OpenGlHelper.lightmapTexUnit);
             GL11.glDisable(GL11.GL_TEXTURE_2D);
             OpenGlHelper.setActiveTexture(OpenGlHelper.defaultTexUnit);
         } catch (Exception e) {
-            if (Tessellator.instance.isDrawing) Tessellator.instance.draw();
+            if (entity != null) {
+                entity.setWorld(null);
+            }
+            if (Tessellator.instance.isDrawing) {
+                Tessellator.instance.draw();
+            }
         }
         BossStatus.bossName = bossName;
         BossStatus.statusBarTime = bossTimeout;
