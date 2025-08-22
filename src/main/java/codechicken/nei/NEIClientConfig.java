6a73e37f32c4bc48221e82e5c6e082b06b1058a3
SB Walker <155262599+sbwaz@users.noreply.github.com>
2025-08-22 20:38:20 +0300

    Stop logging pattern exceptions for NEI search. Remove button allowing to log them too. There's still a config option for that.

--- src/main/java/codechicken/nei/NEIClientConfig.java
+++ src/main/java/codechicken/nei/NEIClientConfig.java
@@ -711,7 +711,6 @@ public class NEIClientConfig {
 
         tag.getTag("inventory.search.logSearchExceptions").setComment("Search exceptions for extended+")
                 .getBooleanValue(false);
-        API.addOption(new OptionToggleButton("inventory.search.logSearchExceptions", true));
 
         String prefixRedefinitions = tag.getTag("inventory.search.prefixRedefinitions").setComment(
                 "Redefine search prefixes by providing a char-to-char map (JSON). The keys are the original prefixes, the values the new ones. Example: { \"$\": \"€\", \"#\": \"+\", \"@\": \"°\", \"%\": \"!\" }")
