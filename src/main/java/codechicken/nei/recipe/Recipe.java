5918bb96a219f70e05c03091fce3530d1deba742
slprime <slprime657@gmail.com>
2026-02-12 23:05:19 +0200

    Generate Single-Recipe Favorites

--- src/main/java/codechicken/nei/recipe/Recipe.java
+++ src/main/java/codechicken/nei/recipe/Recipe.java
@@ -51,6 +51,24 @@ public class Recipe {
             return new RecipeId(extractItem(result), handlerName, extractIngredients(ingredients));
         }
 
+        public static RecipeId of(IRecipeHandler handler, int recipeIndex) {
+            final List<PositionedStack> ingredients = handler.getIngredientStacks(recipeIndex);
+            final String handlerName = GuiRecipeTab.getHandlerInfo(handler).getHandlerName();
+            PositionedStack pStackResult = handler.getResultStack(recipeIndex);
+
+            if (pStackResult == null) {
+                for (PositionedStack otherStack : handler.getOtherStacks(recipeIndex)) {
+                    if (!FluidContainerRegistry.isContainer(otherStack.items[0])
+                            || StackInfo.getFluid(otherStack.items[0]) != null) {
+                        pStackResult = otherStack;
+                        break;
+                    }
+                }
+            }
+
+            return new RecipeId(extractItem(pStackResult), handlerName, extractIngredients(ingredients));
+        }
+
         public static RecipeId of(JsonObject json) {
             String handlerName = null;
             List<ItemStack> ingredients = new ArrayList<>();
