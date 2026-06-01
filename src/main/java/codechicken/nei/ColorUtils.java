6d012f1039824bb798b1951ba1395c2ecd126d02
Ranzu <66495944+Ranzuu@users.noreply.github.com>
2026-06-02 00:14:57 +0200

    spelling

--- src/main/java/codechicken/nei/ColorUtils.java
+++ src/main/java/codechicken/nei/ColorUtils.java
@@ -8,15 +8,15 @@ public class ColorUtils {
 
     public static final ColorResource
     // spotless:off
-        textGray            = color.rgb("textGray",             "#404040"),
-        textLightGray       = color.rgb("textLightGray",        "#606060"),
-        recipeTitle         = color.rgb("recipeTitle",          "#FFFF55"),
-        recipeTitleHover    = color.rgb("recipeTitleHover",     "#FFFFFF"),
-        subsetWidget        = color.rgb("subsetWidget",         "#AA00AA"),
-        recipeBadge         = color.rgb("recipeBadge",          "#FDD835"),
-        buttonLabelNormal   = color.rgb("buttonLabelNormal",    "#E0E0E0"),
-        buttonLabelHover    = color.rgb("buttonLabelHover",     "#FFFFA0"),
-        buttonLabelDisabled = color.rgb("buttonLabelDisabled",  "#601010"),
-        buttonLabelInactive = color.rgb("buttonLabelInactive",  "#A0A0A0");
+        textGray            = color.rgb("textGray",             "0x404040"),
+        textLightGray       = color.rgb("textLightGray",        "0x606060"),
+        recipeTitle         = color.rgb("recipeTitle",          "0xFFFF55"),
+        recipeTitleHover    = color.rgb("recipeTitleHover",     "0xFFFFFF"),
+        subsetWidget        = color.rgb("subsetWidget",         "0xAA00AA"),
+        recipeBadge         = color.rgb("recipeBadge",          "0xFDD835"),
+        buttonLabelNormal   = color.rgb("buttonLabelNormal",    "0xE0E0E0"),
+        buttonLabelHover    = color.rgb("buttonLabelHover",     "0xFFFFA0"),
+        buttonLabelDisabled = color.rgb("buttonLabelDisabled",  "0x601010"),
+        buttonLabelInactive = color.rgb("buttonLabelInactive",  "0xA0A0A0");
     // spotless:on
 }
